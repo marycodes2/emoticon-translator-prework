98b7c9561483e745982d462ad7fbb043b9a1cc19
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-09-13 03:40:58 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,10 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   emoticons_dict = {}
   emoticons_dict[get_meaning] = {}
-  emoticons_dict[get_emoticon]
+  emoticons_dict[get_emoticon] = {}
+  emoticons.each do |meaning, emoticon_array|
+    emoticons_dict[get_meaning][emoticon_array[1]] = meaning
+    emoticons_dict[get_emoticon][emoticon_array[0]] = emoticon_array[1]
 end
 
 puts load_library("./lib/emoticons.yml")
