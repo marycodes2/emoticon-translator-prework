0154804d828784cec6c7f0c677305db2753d4bb0
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-09-13 03:24:43 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,17 +6,16 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(western)
-  load_library("./lib/emoticons.yml")
+  emoticons = load_library("./lib/emoticons.yml")
   emoticons.each do |meaning, emoticon_list|
     if emoticon_list[0] == western
       emoticon_list[1]
     end
   end
-  
 end
 
 def get_english_meaning(emoticon)
-  load_library("./lib/emoticons.yml")
+  emoticons = load_library("./lib/emoticons.yml")
   emoticons.each do |meaning, emoticon_list|
     if emoticon_list[1] == emoticon
       meaning
